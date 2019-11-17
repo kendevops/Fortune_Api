b12525ec11f7a4686b6ccfdde89ab0b7b72329a8
Kenneth Aladi <kennethmichael11@gmail.com>
2019-11-17 06:53:56 +0100

    Added bin and renamed index to app.js so as to fire up my app from an excutable

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const fortunes = require("./data/fortunes");
+
+const app = express();
+
+app.get("/fortunes", (req, res) => {
+  res.json(fortunes);
+});
+
+module.exports = app;
