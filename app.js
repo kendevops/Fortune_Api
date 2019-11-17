3e57a9ee11dbac1f38fdf63a25a75395ee43318d
Kenneth Aladi <kennethmichael11@gmail.com>
2019-11-17 07:59:05 +0100

    Created a get request for random and one fortune

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const fortunes = require("./data/fortunes");
 
 const app = express();
 
-app.get("/fortunes", (req, res) => {
+app.get("/fortunes", (req, res, next) => {
   res.json(fortunes);
+});
+
+app.get("/fortunes/random", (req, res) => {
+  res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get("/fortunes/:id", (req, res) => {
+  res.json(fortunes.find(f => f.id == req.params.id));
 });
 
 module.exports = app;
