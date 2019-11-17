8906648a1c3767149d5c216da32a48fe347fe8a6
Kenneth Aladi <kennethmichael11@gmail.com>
2019-11-17 20:23:41 +0100

    Finally added a delete request to the fortune API

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.get("/fortunes/:id", (req, res) => {
   res.json(fortunes.find(f => f.id == req.params.id));
 });
 
+const writeFortunes = json => {
+  fs.writeFile("./data/fortunes.json", JSON.stringify(json), error =>
+    console.log(error)
+  );
+};
+
 app.post("/fortunes", (req, res) => {
   console.log(req.body);
   const { message, lucky_number, spirit_animal } = req.body;
@@ -32,27 +38,37 @@ app.post("/fortunes", (req, res) => {
     spirit_animal
   });
 
-  fs.writeFile("./data/fortunes.json", JSON.stringify(new_fortunes), error =>
-    console.log(error)
-  );
+  writeFortunes(new_fortunes);
 
   res.json(new_fortunes);
 });
 
 app.put("/fortunes/:id", (req, res) => {
   const { id } = req.params;
-  const { message, lucky_number, spirit_animal } = req.body;
+  // const { message, lucky_number, spirit_animal } = req.body;
   const old_fortune = fortunes.find(f => f.id == id);
 
-  if (message) old_fortune.message = message;
-  if (lucky_number) old_fortune.lucky_number = lucky_number;
-  if (spirit_animal) old_fortune.spirit_animal = spirit_animal;
+  // if (message) old_fortune.message = message;
+  // if (lucky_number) old_fortune.lucky_number = lucky_number;
+  // if (spirit_animal) old_fortune.spirit_animal = spirit_animal;
 
-  fs.writeFile("./data/fortunes.json", JSON.stringify(fortunes), error =>
-    console.log(error)
-  );
+  ["message", "lucky_number", "spirit_animal"].forEach(key => {
+    if (req.body[key]) old_fortune[key] = req.body[key];
+  });
+
+  writeFortunes(fortunes);
 
   res.json(fortunes);
+});
+
+app.delete("/fortunes/:id", (req, res) => {
+  const { id } = req.params;
+
+  const new_fortune = fortunes.filter(f => f.id != id);
+
+  writeFortunes(new_fortune);
+
+  res.json(new_fortune);
 });
 
 module.exports = app;
